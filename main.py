884a56cd8515fc39354bdbaba9881bc6e141da49
bramaaa <nlnuwu123@gmail.com>
2026-01-25 12:52:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Вітаю! Очікуйте розклад. Якщо вам потрібен бот - пишіть @bramaaaaaa ІІІІІІІІІІ")
+    bot.send_message(message.chat.id, "Вітаю! Очікуйте розклад. Якщо вам потрібен бот - пишіть @bramaaaaaa")
 
 
 bot.infinity_polling(none_stop=True)
